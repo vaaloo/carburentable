0e4d13b351604d0e1cea6b801fed264368d042a5
salenson <leo.salenson@outlook.fr>
2025-04-02 08:25:30 +0200

    fix de parseStationPrices, pour que si le prix n'est pas un tableau ca marche quand meme

--- utils/parseStationPrices.ts
+++ utils/parseStationPrices.ts
@@ -1,9 +1,23 @@
-import {Prix} from "../types/Prix";
+import { Prix } from "../types/Prix";
 import Station from "../types/Station";
 
 const parseStationPrices = (station: Station): Prix[] => {
     if (!station.prix) return [];
-    return JSON.parse(station.prix).map((item: { [x: string]: any }) =>
+
+    let prices: any[];
+
+    try {
+        prices = JSON.parse(station.prix);
+    } catch (e) {
+        console.error("Failed to parse prices:", e);
+        return [];
+    }
+
+    if (!Array.isArray(prices)) {
+        prices = [prices];
+    }
+
+    return prices.map(item =>
         Object.keys(item).reduce((acc, key) => {
             // @ts-ignore
             acc[key.replace("@", "")] = item[key];
